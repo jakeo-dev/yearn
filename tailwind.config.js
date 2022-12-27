7b20dd12af49d4d0d05e4404faacb79bf0efa111
Jake O <56329025+jakeorch@users.noreply.github.com>
2022-12-27 06:59:18 -0800

    Design improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,6 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        'slate-350': '#b7c1d1'
-      },
-
       fontFamily: {
         'LexendDeca': ['Lexend Deca', 'sans-serif'],
         'Aclonica': ['Aclonica', 'sans-serif']
